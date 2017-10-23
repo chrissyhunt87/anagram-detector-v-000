ece1e037bcd380690ce51efa67eef4726f5f4a11
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-23 00:20:56 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,6 @@ class Anagram
   end
 
   def match(possible_anagrams)
-    # sorted_word_array = @word.split().sort
     possible_anagrams.select {|a| a.split().sort.join == @word.split().sort.join}
   end
 
