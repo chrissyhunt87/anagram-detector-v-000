b604d4b8a4b4606ae8f65be639490cbcc4af6733
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-23 00:19:05 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
 
   def match(possible_anagrams)
     sorted_word_array = @word.split().sort
-    possible_anagrams.select {|a| a.split().sort == sorted_word_array}
+    possible_anagrams.select {|a| a.split().sort == @word.split().sort}
   end
 
 end
