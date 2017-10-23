e39b6e4ef8cc30702aa0c6599855e52b37ee3825
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-23 00:13:55 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,12 +3,11 @@ class Anagram
 
   def initialize(word)
     @word = word
-    @sorted_word_array = word.split().sort
   end
 
   def match(possible_anagrams)
     possible_anagrams_array = possible_anagrams.split(" ")
-    
+
   end
 
 end
